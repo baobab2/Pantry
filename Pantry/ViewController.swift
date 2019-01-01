9a087dd8f6b19e7273fa233bf4ff3b72638b89fb
Baobab2 <fabrizio@baomacbookpro.local>
2019-01-01 19:13:22 +0100

    Initial Commit

---
+++ Pantry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pantry
+//
+//  Created by Fabrizio Imperio on 01.01.19.
+//  Copyright © 2019 Baonet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
